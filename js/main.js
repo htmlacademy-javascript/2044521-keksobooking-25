c9e76b867c27dd8a2425dc51708c27b74dc1b2a4
Olga Lokko <lokko.olga@gmail.com>
2022-02-25 20:18:15 +0200

    Дополнения

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 function getRandomInt(minNumber, maxNumber) {
   if (minNumber >= 0 && minNumber < maxNumber ){
     return Math.floor(Math.random() * (maxNumber - minNumber + 1)) +minNumber;
@@ -9,15 +7,11 @@ function getRandomInt(minNumber, maxNumber) {
 getRandomInt(10, 90);
 
 
-
 function getRandomFloat(minNumber, maxNumber, numberOfDecimal) {
 
   if (minNumber >= 0 && minNumber <= maxNumber){
-    return Math.random().toFixed(numberOfDecimal) * (maxNumber - minNumber + 1) + minNumber;
+    return (Math.random() * (maxNumber - minNumber + 1) + minNumber).toFixed(numberOfDecimal);
   }
   throw new Error('Invalid numbers');
 }
-
-
-
 getRandomFloat(3, 90, 3);
