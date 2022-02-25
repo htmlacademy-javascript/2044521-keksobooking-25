d232f23cf04e552eff1e35d1f10a665ade0b9989
Olga Lokko <lokko.olga@gmail.com>
2022-02-25 13:23:31 +0200

    Доработка кода функций

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-//Функция возвращающая случайное целое число из переданного диапазона включительно.
 
-function getRandomInt(minNum, maxNum) {
-  if (minNum >= 0 && minNum<maxNum ){
-    return Math.floor(Math.random() * (maxNum - minNum + 1)) +minNum;
-  } else {
-    console.log ('Invalid numbers');
+
+function getRandomInt(minNumber, maxNumber) {
+  if (minNumber >= 0 && minNumber < maxNumber ){
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) +minNumber;
   }
-
+  throw new Error('Invalid numbers');
 }
-getRandomInt();
+getRandomInt(10, 90);
 
-//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 
-function getRandomFloat(minNum, maxNum) {
-  if (minNum >= 0 && minNum <= maxNum){
-    return Math.random() * (maxNum - minNum + 1) + minNum;
-  } else {
-    console.log ('Invalid numbers');
+
+function getRandomFloat(minNumber, maxNumber, numberOfDecimal) {
+
+  if (minNumber >= 0 && minNumber <= maxNumber){
+    return Math.random().toFixed(numberOfDecimal) * (maxNumber - minNumber + 1) + minNumber;
   }
+  throw new Error('Invalid numbers');
 }
 
 
-getRandomFloat().toFixed();
+
+getRandomFloat(3, 90, 3);
